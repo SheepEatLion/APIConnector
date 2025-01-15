69541ac99a571063986c55a658a6282c89f761ba
junyeong <gowjr207@gmail.com>
2025-01-15 14:32:23 +0900

    feat: QueryParamUtils and ObjectMapperProvider

---
+++ src/main/java/com/auff/apiConnector/config/ObjectMapperProvider.java
@@ -0,0 +1,21 @@
+package com.auff.apiConnector.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ObjectMapperProvider {
+  private static final ObjectMapper INSTANCE = objectMapper();
+
+  private static ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    return mapper;
+  }
+
+  public static ObjectMapper getInstance() {
+    return INSTANCE;
+  }
+}
---
+++ src/main/java/com/auff/apiConnector/util/QueryParamUtils.java
@@ -0,0 +1,19 @@
+package com.auff.apiConnector.util;
+
+import com.auff.apiConnector.config.ObjectMapperProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+public class QueryParamUtils {
+  private static final ObjectMapper objectMapper = ObjectMapperProvider.getInstance();
+
+  public static MultiValueMap<String, String> toQueryParams(Object object) {
+    Map<String, Object> map = objectMapper.convertValue(object, Map.class);
+    MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+    map.forEach((key, value) -> multiValueMap.add(key, value != null ? value.toString() : null));
+
+    return multiValueMap;
+  }
+}
