d51e10e36e3999abffdda7c8f26129c424793afe
junyeong <gowjr207@gmail.com>
2025-01-15 14:31:44 +0900

    test: QueryParamUtilsTest

---
+++ src/test/java/com/auff/apiConnector/QueryParamUtilsTest.java
@@ -0,0 +1,41 @@
+package com.auff.apiConnector;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.auff.apiConnector.client.nasa.NasaQueryParams;
+import com.auff.apiConnector.util.QueryParamUtils;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.MultiValueMap;
+
+class QueryParamUtilsTest {
+
+  @Test
+  void shouldReturnDefaultValue_WhenParamIsNull() {
+    NasaQueryParams queryParams = NasaQueryParams.builder().build();
+
+    MultiValueMap<String, String> queryParamsMap = QueryParamUtils.toQueryParams(queryParams);
+
+    assertEquals("DEMO_KEY", queryParamsMap.getFirst("api_key"));
+  }
+
+  @Test
+  void shouldReturnString_WhenParamIsLocalDateType() {
+    NasaQueryParams queryParams = NasaQueryParams.builder().specificDate(LocalDate.now()).build();
+
+    MultiValueMap<String, String> queryParamsMap = QueryParamUtils.toQueryParams(queryParams);
+
+    assertEquals("DEMO_KEY", queryParamsMap.getFirst("api_key"));
+    assertEquals(String.valueOf(LocalDate.now()), queryParamsMap.getFirst("date"));
+  }
+
+  @Test
+  void shouldReturnString_WhenParamIsInteger() {
+    NasaQueryParams queryParams = NasaQueryParams.builder().numberOfRandomImages(2).build();
+
+    MultiValueMap<String, String> queryParamsMap = QueryParamUtils.toQueryParams(queryParams);
+
+    assertEquals("DEMO_KEY", queryParamsMap.getFirst("api_key"));
+    assertEquals("2", queryParamsMap.getFirst("count"));
+  }
+}
