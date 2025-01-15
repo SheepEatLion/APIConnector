cc1f8864b5efd25f81616b18f4983c1f6c6acdad
junyeong <gowjr207@gmail.com>
2025-01-15 16:55:41 +0900

    chore: delete openFeign

--- src/main/java/com/auff/apiConnector/ApiConnectorApplication.java
+++ src/main/java/com/auff/apiConnector/ApiConnectorApplication.java
@@ -2,10 +2,8 @@ package com.auff.apiConnector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class ApiConnectorApplication {
 
 	public static void main(String[] args) {
