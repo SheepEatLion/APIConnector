f47dc059a8f7322b20252d1b4ed4aead612b342f
junyeong <gowjr207@gmail.com>
2025-01-17 16:00:28 +0900

    feat: add photo controller

--- src/main/java/com/auff/apiConnector/adapters/controller/PhotoController.java
+++ src/main/java/com/auff/apiConnector/adapters/controller/PhotoController.java
@@ -1,25 +1,23 @@
 package com.auff.apiConnector.adapters.controller;
 
+import com.auff.apiConnector.application.dto.PhotoRequest;
 import com.auff.apiConnector.application.dto.PhotoResponse;
-import com.auff.apiConnector.domain.model.Provider;
 import com.auff.apiConnector.ports.inbound.GetPhotoUseCase;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "sheep-eat-lion/v1/photos")
+@RequestMapping(value = "apis/v1/photos")
 public class PhotoController {
 
   private final GetPhotoUseCase getPhotoUseCase;
 
   @GetMapping
-  public ResponseEntity<PhotoResponse> getPhoto(@RequestParam Provider provider, @RequestParam LocalDate takenDate) {
-    return ResponseEntity.ok(getPhotoUseCase.excute());
+  public ResponseEntity<PhotoResponse> getPhoto(PhotoRequest photoRequest) {
+    return ResponseEntity.ok(getPhotoUseCase.excute(photoRequest));
   }
 }
