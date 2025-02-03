6d1dc88151ad3c1e24b508089c9753f7166ccb75
junyeong <gowjr207@gmail.com>
2025-02-03 10:21:58 +0900

    refactor: web client for NASA
    * 웹클라이언트 호출부를 프라이빗으로 변경
    * 포트 인터페이스를 오버라이드한 메소드를 퍼블릭으로 구현
    * 플럭스 구조로 변경

--- src/main/java/com/auff/apiConnector/adapters/client/NasaClient.java
+++ src/main/java/com/auff/apiConnector/adapters/client/NasaClient.java
@@ -1,8 +1,13 @@
 package com.auff.apiConnector.adapters.client;
 
-import com.auff.apiConnector.adapters.client.dto.NasaQueryParams;
+import com.auff.apiConnector.adapters.client.dto.NasaRequest;
 import com.auff.apiConnector.adapters.client.dto.NasaResponse;
+import com.auff.apiConnector.application.dto.PhotoQuery;
+import com.auff.apiConnector.application.ports.outbound.LoadPhotoPort;
+import com.auff.apiConnector.domain.model.Photo;
+import com.auff.apiConnector.domain.model.Provider;
 import com.auff.apiConnector.infra.util.QueryParamUtils;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,14 +15,37 @@ import reactor.core.publisher.Flux;
 
 @Service
 @RequiredArgsConstructor
-public class NasaClient {
+public class NasaClient implements LoadPhotoPort {
 
   private final WebClient toNasa;
 
-  public Flux<NasaResponse> getPlanetaryImageBy(NasaQueryParams nasaQueryParams) {
+  @Override
+  public  Flux<Photo> loadPhoto(PhotoQuery query) {
+    NasaRequest request = NasaRequest.builder()
+        .specificDate(query.getTakenDate())
+        .startDate(null)
+        .endDate(null)
+        .numberOfRandomImages(null)
+        .build();
 
-    return toNasa.get().uri(uriBuilder -> uriBuilder.path("/planetary/apod")
-            .queryParams(QueryParamUtils.toQueryParams(nasaQueryParams)).build())
-        .retrieve().bodyToFlux(NasaResponse.class);
+    Flux<NasaResponse> response = getPlanetaryImageBy(request);
+
+    return response.map(res -> Photo.builder()
+        .title(res.titleOfPhoto())
+        .provider(Provider.NASA)
+        .explanation(res.explanation())
+        .takenDate(LocalDate.parse(res.photoTakenDate()))
+        .takenBy("Empty")
+        .link(res.hdImageUrl())
+        .copyright(res.copyright()).build());
+  }
+
+  private Flux<NasaResponse> getPlanetaryImageBy(NasaRequest nasaRequest) {
+
+    return toNasa.get()
+        .uri(uriBuilder -> uriBuilder.path("/planetary/apod")
+            .queryParams(QueryParamUtils.toQueryParams(nasaRequest)).build())
+        .retrieve()
+        .bodyToFlux(NasaResponse.class);
   }
 }
